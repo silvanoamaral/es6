be3b45c5cdfd475b4985131b0f171c1afb0cbe55
silvanoamaral <silvano.amaral9@gmail.com>
2018-03-18 13:19:57 -0300

    first commit

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var teste = 'Hello World!';
+console.log(teste);
+
+var func1 = function func1(x) {
+  return x * x;
+};
+
+var func2 = function func2(x, y) {
+  return x + y;
+};
---
+++ index.js
@@ -0,0 +1,6 @@
+let teste = 'Hello World!';
+console.log(teste);
+
+const func1 = x => x * x; 
+ 
+const func2 = (x, y) => { return x + y; }; 
